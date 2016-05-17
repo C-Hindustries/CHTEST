66668f217520ad39c99727a77ec03c3722f35c9f
cha014 <c-h-F-15@kcs.org.uk>
2016-05-17 13:56:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+var rocks = 0;
+
 
 function buttonAlert() {
   alert("Hey, I'm a cringey alert!");
+}
+
+function getRock() {
+  rocks += 1;
+  document.getElementById("rockNum").innerHTML = "rocks";
 }
